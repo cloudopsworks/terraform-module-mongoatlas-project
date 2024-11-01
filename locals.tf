5217abed4972379a73afcd3d464b2013392db433
Cristian Beraha <cristian@cloudops.works>
2024-11-01 17:17:27 -0300

    chore: naming without spokedef

--- locals.tf
+++ locals.tf
@@ -6,6 +6,7 @@
 locals {
   # No region at this level
   system_name       = format("%s-%s-%s-%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type), var.spoke_def)
+  system_name_plain = format("%s-%s-%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type))
   system_name_short = format("%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 3), substr(lower(var.org.environment_name), 0, 3), substr(lower(var.org.environment_type), 0, 4), var.spoke_def)
   all_tags          = merge(module.tags.locals.common_tags, var.extra_tags)
   secret_store_path = format("/%s/%s/%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type))
--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "mongodbatlas_organizations" "this" {
 }
 
 resource "mongodbatlas_project" "this" {
-  name   = var.name != "" ? var.name : format("%s-%s", var.name_prefix, local.system_name)
+  name   = var.name != "" ? var.name : format("%s-%s", var.name_prefix, local.system_name_plain)
   org_id = var.organization_id != "" ? var.organization_id : data.mongodbatlas_organizations.this[0].id
   tags = { for k, v in local.all_tags :
     k => replace(v, "/[/$%&#]/", "+")
